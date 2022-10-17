b7f1cfe386b613f6c10bd3126bd6a1c9b5032293
scrumhum <makayla.kuhnke@gmail.com>
2022-10-16 17:07:06 -0700

    committed

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,7 +1,26 @@
 import java.util.Scanner;
 
 public class MyMath {
-
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter two integers to find GCF: ");
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println("The GCF is " + gcf(a, b));
+
     }
-}
+
+    public static int gcf(int a, int b) {
+        while (b != 0)
+            if (a > b) {
+                int c = b;
+                c = b;
+                b = a % b;
+                a = c;
+            } else {
+                b = b % a;
+            }
+        return a;
+    }
+    }
+
